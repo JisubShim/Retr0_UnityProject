b3cbd99ddb32d5beb3fb07de11ff3941ba6b08b9
Jisub Shim <jisub5322@naver.com>
2024-03-14 23:49:15 +0900

    Update Bullet

---
+++ Dodge/Assets/Bullet.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f; // 탄알 이동 속력
+    private Rigidbody bulletRigidbody; // 이동에 사용할 리지드바디 컴포넌트
+    void Start()
+    {
+        bulletRigidbody = GetComponent<Rigidbody>();
+        bulletRigidbody.velocity = transform.forward * speed;
+
+        Destroy(gameObject, 3f); // 3초 뒤에 자신의 게임 오브젝트 파괴
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            PlayerController playerController = other.GetComponent<PlayerController>();
+
+            if(playerController != null)
+            {
+                playerController.Die();
+            }
+        }
+    }
+}
